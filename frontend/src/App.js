30b17fb1229600c0d00c16bce74307e390dc69bb
GreenBay <156261960+GreenBay8@users.noreply.github.com>
2026-02-28 02:53:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "PASTE_BACKEND_URL_LATER";
+const API = "https://hrms-lite-2-iege.onrender.com";
 
 function App() {
   const [employees, setEmployees] = useState([]);
